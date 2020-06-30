6c54b994bbe56a0e6284d8385ed06c3dbae5e6d0
sooyoungYun <67577148+dnstndud123@users.noreply.github.com>
2020-06-30 14:40:36 +0900

    움직이기
    점프, 좌우 이동

--- Assets/script/Character2D.cs
+++ Assets/script/Character2D.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class Character2D : MonoBehaviour
 {
+    protected Rigidbody2D rigid;
+    protected Animator anim;
+    public float forceY;
+    public float maxForce;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        anim.SetFloat("velocity", Mathf.Abs(rigid.velocity.x));
+    }
+
+    public void Flip(bool right)
+    {
+        Vector2 scale = transform.localScale;
         
+        if (right == true)
+        {
+            float scaleX = Mathf.Abs(scale.x);
+            transform.localScale = new Vector2(scaleX, scale.y);
+        }
+        else
+        {
+            float scaleX = Mathf.Abs(scale.x);
+            transform.localScale = new Vector2(-scaleX, scale.y);
+        }
+    }
+    public void Jump()
+    {
+        bool isJump = anim.GetBool("jump");
+
+        if (isJump == true)
+            return;
+
+        anim.SetBool("jump", true);
+        rigid.AddForce(new Vector2(0, forceY));
+
+
+            
+
+            Vector2 velY = rigid.velocity;
+            float limitY = Mathf.Min(maxForce, velY.y);
+
+            rigid.velocity = new Vector2(velY.x, limitY);
+
+
+
+    }
+    public void Crouch(bool isCrouch)
+    {
+        
+        if (isCrouch == true)
+        {
+            anim.SetBool("crouch", true);
+            rigid.velocity = new Vector2(rigid.velocity.x, 0);
+        }
+        else
+        {
+            anim.SetBool("crouch", false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "ground")
+        {
+            anim.SetBool("jump", false);
+        }
     }
 }
--- Assets/script/PlayerControl.cs
+++ Assets/script/PlayerControl.cs
@@ -9,30 +9,56 @@ public class PlayerControl : MonoBehaviour
     public float speedX;
     public float maxSpeed;
 
+
     public Character2D char2D;
     public Rigidbody2D rigid;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = maxHp;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            char2D.Crouch(true);
+            
+            if (Input.GetKey(KeyCode.LeftArrow))
+                char2D.Flip(false);
+            if (Input.GetKey(KeyCode.RightArrow))
+                char2D.Flip(true);
+            return;
+        }
+        if(Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            char2D.Crouch(false);
+        }
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             rigid.AddForce(new Vector2(-speedX, 0));
+            char2D.Flip(false);
         }
         if(Input.GetKey(KeyCode.RightArrow))
         {
             rigid.AddForce(new Vector2(speedX, 0));
+            char2D.Flip(true);
         }
 
-        Vector2 vel = rigid.velocity;
-        float limit = Mathf.Min(maxSpeed, vel.x);
-        limit = Mathf.Max(-maxSpeed,limit);
-        rigid.velocity = new Vector2(limit, vel.y);
+        Vector2 velX = rigid.velocity;
+        float limitX = Mathf.Min(maxSpeed, velX.x);
+        limitX = Mathf.Max(-maxSpeed,limitX);
+        rigid.velocity = new Vector2(limitX, velX.y);
+
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            char2D.Jump();
+            
+        }
+
+        
 
         
     }
