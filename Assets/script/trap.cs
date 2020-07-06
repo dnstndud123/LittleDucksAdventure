daae5a15b69f997bb65993fa30d712949019cd3e
sooyoungYun <67577148+dnstndud123@users.noreply.github.com>
2020-07-06 11:10:38 +0900

    스테이지2 만들기

--- Assets/script/trap.cs
+++ Assets/script/trap.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
     protected Character2D player;
-
+    
 
 
     // Start is called before the first frame update
     protected void Start()
     {
         player = player = FindObjectOfType<Character2D>();
+        
     }
     protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
