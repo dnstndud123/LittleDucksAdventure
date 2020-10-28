cd742b2435ce59dfd677ce573ca6035f49c1c0bd
sooyoungYun <67577148+dnstndud123@users.noreply.github.com>
2020-10-28 17:57:01 +0900

    스테이지 10 제작중(새로운 함정)

---
+++ Assets/script/Object/TrapFake.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapFake : MonoBehaviour
+{
+    public GameObject obj1;
+    public GameObject obj2;
+    public float posX;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            
+            
+            StartCoroutine(Wide());
+        }
+    }
+    IEnumerator Wide()
+    {
+        BoxCollider2D col = GetComponent<BoxCollider2D>();
+        obj1.transform.Translate(new Vector2(-posX, 0));
+        obj2.transform.Translate(new Vector2(posX, 0));
+
+        yield return new WaitForSeconds(0.1f);
+        col.enabled = false;
+        
+    }
+}
